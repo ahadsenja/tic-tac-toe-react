467bf58a7386850f7118dc97f4b8c78fc9c04e89
ahadsenja <hamdanilm96@gmail.com>
2023-07-14 22:58:08 +0800

    chore: change component from calling Board, to calling Game component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import Board from './components/Board'
+'use client'
+import Game from './components/Game'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24 gap-4">
       <h1>Tic Tac Toe Game Exercise with Nextjs</h1>
-      <Board />
+      <Game />
     </main>
   )
 }
