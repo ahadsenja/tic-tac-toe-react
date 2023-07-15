e94733eab2ed257e72325eb96804af19b4a53a81
ahadsenja <hamdanilm96@gmail.com>
2023-07-15 10:30:45 +0800

    chore: update the page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-'use client'
-import Game from './components/Game'
+"use client";
+import Game from "./components/Game";
 
 export default function Home() {
   return (
@@ -7,5 +7,5 @@ export default function Home() {
       <h1>Tic Tac Toe Game Exercise with Nextjs</h1>
       <Game />
     </main>
-  )
+  );
 }
